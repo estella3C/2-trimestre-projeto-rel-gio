353036b13f66072882aa085c1f1b3fbbe987c32b
estella3C <estella.nascimento@escola.pr.gov.br>
2025-05-26 10:43:45 -0300

    kook

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const botoes - document.querySelectorAll(".botoes");
 .log(botoes)
-for(let i=0; i< botoes.lenght; i++){
 
+for(let i=0; i< botoes.lenght; i++){
 botoes[i].onclick = function(){
      botoes[i].claslist.add("ativo");
 }
